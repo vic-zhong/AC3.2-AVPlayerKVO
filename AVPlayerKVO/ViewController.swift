fe09dab249022fc2f6fb7da88f1adfe1c028ca5a
Jason Gresh <jrgresh@gmail.com>
2017-01-25 17:42:37 -0500

    Exploring mp3s

--- AVPlayerKVO/ViewController.swift
+++ AVPlayerKVO/ViewController.swift
@@ -22,23 +22,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let url = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8") {
-            let playerItem = AVPlayerItem(url: url)
-            
-            playerItem.addObserver(self, forKeyPath: "status", options: .new, context: &kvoContext)
-            
-            player = AVPlayer(playerItem: playerItem)
-            
-            let playerLayer = AVPlayerLayer(player: player)
-            //playerLayer.frame = self.videoContainer.bounds
-            self.videoContainer.layer.addSublayer(playerLayer)
-            
-            let timeInterval = CMTime(value: 1, timescale: 2)
-            player.addPeriodicTimeObserver(forInterval: timeInterval, queue: DispatchQueue.main, using: { (time: CMTime) in
-                print(time)
-                self.updatePositionSlider()
-            })
-        }
+        loadAssetFromFile(urlString: "debussy.mp3")
+        
+//        if let url = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8") {
+//            let playerItem = AVPlayerItem(url: url)
+//            
+//            playerItem.addObserver(self, forKeyPath: "status", options: .new, context: &kvoContext)
+//            
+//            player = AVPlayer(playerItem: playerItem)
+//            
+//            let playerLayer = AVPlayerLayer(player: player)
+//            //playerLayer.frame = self.videoContainer.bounds
+//            self.videoContainer.layer.addSublayer(playerLayer)
+//            
+//            let timeInterval = CMTime(value: 1, timescale: 2)
+//            player.addPeriodicTimeObserver(forInterval: timeInterval, queue: DispatchQueue.main, using: { (time: CMTime) in
+//                print(time)
+//                self.updatePositionSlider()
+//            })
+//        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -60,6 +62,24 @@ class ViewController: UIViewController {
         self.positionSlider.value = currentPlace
     }
     
+    func loadAssetFromFile(urlString: String) {
+        guard let dot = urlString.range(of: ".") else { return }
+        let fileParts = (resource: urlString.substring(to: dot.lowerBound), extension: urlString.substring(from: dot.upperBound))
+        
+        if let fileURL = Bundle.main.url(forResource: fileParts.resource, withExtension: fileParts.extension) {
+            let asset = AVURLAsset(url: fileURL)
+            let playerItem = AVPlayerItem(asset: asset)
+            playerItem.addObserver(self, forKeyPath: "status", options: .new, context: &kvoContext)
+            self.player = AVPlayer(playerItem: playerItem)
+            
+            let timeInterval = CMTime(value: 1, timescale: 2)
+            player.addPeriodicTimeObserver(forInterval: timeInterval, queue: DispatchQueue.main, using: { (time: CMTime) in
+                print(time)
+                self.updatePositionSlider()
+            })
+        }
+    }
+
     // MARK: - KVO
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
@@ -80,6 +100,8 @@ class ViewController: UIViewController {
         let newPosition = Double(sender.value) * item.duration.seconds
         
         player.seek(to: CMTime(seconds: newPosition, preferredTimescale: 1000))
+        
+        player.playImmediately(atRate: userPlayRate)
     }
     
     @IBAction func rateChange(_ sender: UISlider) {
